89cb7dcf1eb30141e096a6e5bf3d74c5d697b3fe
Luciano Zembruzki <40046862+zembruzkill@users.noreply.github.com>
2023-09-13 04:09:33 -0300

    fix menu

--- src/components/layout/headers/Header.tsx
+++ src/components/layout/headers/Header.tsx
@@ -58,7 +58,7 @@ export default function Example() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#172645] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gradient-to-r from-[#172645] to-[#ff80b5] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
             <div className=''>
