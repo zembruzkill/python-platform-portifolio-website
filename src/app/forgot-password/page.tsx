1a2b074b911cb9b86b8fc9c086c3ca9401d32bb7
Luciano Zembruzki <40046862+zembruzkill@users.noreply.github.com>
2023-09-14 02:50:38 -0300

    added reset password

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
-const SignIn = () => {
+const ForgotPassword = () => {
   const router = useRouter();
     return (
       <div>
@@ -144,9 +144,10 @@ const SignIn = () => {
 
               <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
                 <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-                  <h2 className="mb-6 text-2xl font-bold text-indigo-600 dark:text-white sm:text-title-xl2">
-                    Faça login
+                  <h2 className="mb-1 text-2xl font-bold text-indigo-600 dark:text-white sm:text-title-xl2">
+                    Redefinir senha
                   </h2>
+                  <h4 className="text-sm mb-8 italic">Digite seu e-mail para receber um link de redefinição de senha.</h4>
 
                   <form>
                     <div className="mb-4">
@@ -180,111 +181,12 @@ const SignIn = () => {
                       </div>
                     </div>
 
-                    <div className="mb-2">
-                      <label className="block font-medium text-black dark:text-white">
-                        Senha
-                      </label>
-                      <div className="relative">
-                        <input
-                          type="password"
-                          placeholder="Senha"
-                          className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                        />
-
-                        <span className="absolute right-4 top-4">
-                          <svg
-                            className="fill-current"
-                            width="22"
-                            height="22"
-                            viewBox="0 0 22 22"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <g opacity="0.5">
-                              <path
-                                d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
-                                fill=""
-                              />
-                              <path
-                                d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
-                                fill=""
-                              />
-                            </g>
-                          </svg>
-                        </span>
-                      </div>
-
-                      <div className="pt-2 mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
-                        <input
-                          className="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                          type="checkbox"
-                          value=""
-                          id="checkboxDefault" />
-                        <label
-                          className="text-sm italic inline-block pl-[0.15rem] hover:cursor-pointer"
-                          htmlFor="checkboxDefault">
-                          Lembrar senha
-                        </label>
-                      </div>
-                    </div>
-
                     <div className="mb-5">
                       <input
                         type="submit"
-                        value="Login"
+                        value="Enviar link de redefinição de senha"
                         className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-bold text-black transition hover:bg-opacity-60"
                       />
-                      <div onClick={() => router.push('/forgot-password')} className="flex items-center pt-2">
-                      <button className="pr-2 hover:text-indigo-600">Esqueceu sua senha?</button>
-                      <FaArrowRight onClick={() => router.push('/forgot-password')}  size={20} color="rgb(79 70 229)" />
-                    </div>
-                    </div>
-                  
-                    <button className="flex w-full font-bold text-black items-center justify-center gap-3.5 rounded-lg border border-stroke bg-gray p-4 hover:bg-opacity-50 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50">
-                      <span>
-                        <svg
-                          width="20"
-                          height="20"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <g clipPath="url(#clip0_191_13499)">
-                            <path
-                              d="M19.999 10.2217C20.0111 9.53428 19.9387 8.84788 19.7834 8.17737H10.2031V11.8884H15.8266C15.7201 12.5391 15.4804 13.162 15.1219 13.7195C14.7634 14.2771 14.2935 14.7578 13.7405 15.1328L13.7209 15.2571L16.7502 17.5568L16.96 17.5774C18.8873 15.8329 19.9986 13.2661 19.9986 10.2217"
-                              fill="#4285F4"
-                            />
-                            <path
-                              d="M10.2055 19.9999C12.9605 19.9999 15.2734 19.111 16.9629 17.5777L13.7429 15.1331C12.8813 15.7221 11.7248 16.1333 10.2055 16.1333C8.91513 16.1259 7.65991 15.7205 6.61791 14.9745C5.57592 14.2286 4.80007 13.1801 4.40044 11.9777L4.28085 11.9877L1.13101 14.3765L1.08984 14.4887C1.93817 16.1456 3.24007 17.5386 4.84997 18.5118C6.45987 19.4851 8.31429 20.0004 10.2059 19.9999"
-                              fill="#34A853"
-                            />
-                            <path
-                              d="M4.39899 11.9777C4.1758 11.3411 4.06063 10.673 4.05807 9.99996C4.06218 9.32799 4.1731 8.66075 4.38684 8.02225L4.38115 7.88968L1.19269 5.4624L1.0884 5.51101C0.372763 6.90343 0 8.4408 0 9.99987C0 11.5589 0.372763 13.0963 1.0884 14.4887L4.39899 11.9777Z"
-                              fill="#FBBC05"
-                            />
-                            <path
-                              d="M10.2059 3.86663C11.668 3.84438 13.0822 4.37803 14.1515 5.35558L17.0313 2.59996C15.1843 0.901848 12.7383 -0.0298855 10.2059 -3.6784e-05C8.31431 -0.000477834 6.4599 0.514732 4.85001 1.48798C3.24011 2.46124 1.9382 3.85416 1.08984 5.51101L4.38946 8.02225C4.79303 6.82005 5.57145 5.77231 6.61498 5.02675C7.65851 4.28118 8.9145 3.87541 10.2059 3.86663Z"
-                              fill="#EB4335"
-                            />
-                          </g>
-                          <defs>
-                            <clipPath id="clip0_191_13499">
-                              <rect width="20" height="20" fill="white" />
-                            </clipPath>
-                          </defs>
-                        </svg>
-                      </span>
-                      Login com Google
-                    </button>
-
-                    <div className=" flex mt-6 text-center text-black items-center">
-                      <p>
-                        Ainda não tem uma conta?{" "}
-                        <Link href="/sign-up" className="text-black font-bold pr-1 hover:text-indigo-600">
-                          Cadastre agora
-                        </Link>
-                      </p>
-                      <FaArrowRight onClick={() => router.push('/sign-up')}  size={20} color="rgb(79 70 229)" />
                     </div>
                   </form>
                 </div>
@@ -301,4 +203,4 @@ const SignIn = () => {
     );
   };
   
-  export default SignIn;
+  export default ForgotPassword;
