b899fe9f03da15722bd203aa0b384fc8f6d3403f
Luciano Zembruzki <40046862+zembruzkill@users.noreply.github.com>
2023-09-16 12:53:34 -0300

    fix robots block

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/dashboard/*', '/api/*', '/_next/*', '/auth/*'],
+      disallow: ['/dashboard/*', '/api/*', '/auth/*'],
     },
     sitemap: 'https://cursopython.pro/sitemap.xml',
   }
